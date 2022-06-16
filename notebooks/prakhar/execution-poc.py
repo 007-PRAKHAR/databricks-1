0ac9446a5a108278a3f95e7d7e9b7899c1fc268b
007-PRAKHAR <73653050+007-PRAKHAR@users.noreply.github.com>
2022-06-16 15:21:39 +0530

    asdfghj

--- notebooks/prakhar/execution-poc.py
+++ notebooks/prakhar/execution-poc.py
@@ -1,11 +1,1 @@
-# Databricks notebook source
-def isPalindrome(s):
-    return s == s[::-1]
-
-list1 = ['nayan','suraj','malayalam','navin']
-for item in list1:
-    ans = isPalindrome(item)
-    if ans:
-        print(f"{item} is palindrome")
-    else:
-        print(f"{item} is not palindrome")
+pip insall pypi
