c45cc7c5b9b8e463e1cdcb555612c64a93471639
007-PRAKHAR <73653050+007-PRAKHAR@users.noreply.github.com>
2022-06-23 11:48:03 +0530

    updated file3.py

--- notebooks/folder3/file3.py
+++ notebooks/folder3/file3.py
@@ -1,1 +1,10 @@
-#commit 3
+def isPalindrome(s):
+    return s == s[::-1]
+
+list1 = ['nayan','suraj','malayalam','navin']
+for item in list1:
+    ans = isPalindrome(item)
+    if ans:
+        print(f"{item} is palindrome")
+    else:
+        print(f"{item} is not palindrome")
